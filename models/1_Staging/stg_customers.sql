00e68b9c1efd373586fa18de87ca6d78d9238df8
Your NameGopal <you@example.comgopal.msbi4@gmail.com>
2026-05-16 13:03:27 -0500

    added staging model

---
+++ models/1_Staging/stg_customers.sql
@@ -0,0 +1,17 @@
+WITH source_customer AS (
+    SELECT * 
+    FROM {{ source('snowflake_sample_data', 'CUSTOMER') }}
+)
+, final AS (
+    SELECT 
+        c_custkey,
+        c_name,
+        c_address,
+        c_nationkey,
+        c_phone,
+        c_acctbal,
+        c_mktsegment,
+        c_comment
+    FROM source_customer
+)
+SELECT * FROM final
